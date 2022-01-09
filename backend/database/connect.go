3070401be195fe6c5264d22b21ec4853fe9dde7b
Katsushi21 <sheoaspendie@gmail.com>
2022-01-09 21:41:28 +0900

    update: バックエンドにデータベース接続処理を追加 update: データベース初期構築の記述を変更

---
+++ backend/database/connect.go
@@ -0,0 +1,68 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/handler"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func connect() {
+	err := godotenv.Load(fmt.Sprintf("../%s.env", os.Getenv("GO_ENV")))
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	database_name := os.Getenv("DB_DATABASE_NAME")
+
+	dsn := fmt.Sprintf(`%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local`,
+		user,
+		password,
+		host,
+		port,
+		database_name,
+	)
+	sqlDB, err := sql.Open("mysql", dsn)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	gormDB, err := gorm.Open(mysql.New(mysql.Config{
+		Conn: sqlDB,
+	}), &gorm.Config{})
+	if err != nil {
+		panic(err.Error())
+	}
+	if gormDB == nil {
+		panic(err)
+	}
+
+	db, err := gormDB.DB()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer func() {
+		if db != nil {
+			if err := db.Close(); err != nil {
+				panic(err)
+			}
+		}
+	}()
+
+	http.Handle("/", handler.Playground("GraphQL playground", "/query"))
+	http.Handle("/query", handler.GraphQL(backend.NewExecutableSchema(backend.Config{Resolvers: &backend.Resolver{DB: db}})))
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
