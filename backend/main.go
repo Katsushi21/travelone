d3b8e6f205f75fb39c56b574a35636d237ee7487
Katsushi21 <sheoaspendie@gmail.com>
2022-03-29 23:29:50 +0900

    update: マイグレーションモデルの作成

--- backend/main.go
+++ backend/main.go
@@ -1,63 +1,71 @@
 package main
 
-// import (
-// 	"context"
-// 	"fmt"
+import (
+	"context"
+	"fmt"
 
-// 	// "github.com/Katsushi21/traveling_alone/graphql/generated"
-// 	// "github.com/gin-gonic/gin"
+	"github.com/99designs/gqlgen/example/federation/reviews/graph"
+	"github.com/99designs/gqlgen/example/starwars/generated"
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
 
-// 	"github.com/99designs/gqlgen/graphql/handler"
-// 	"github.com/99designs/gqlgen/graphql/playground"
-// )
+func main() {
+	// Setting up Gin
+	r := gin.Default()
+	r.POST("/query", graphqlHandler())
+	r.GET("/", playgroundHandler())
+	r.Run()
+	db, err := gorm.Open(postgres.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	// Migrate the schema
+	// db.AutoMigrate(&Product{})
+}
 
 // // Defining the Graphql handler
-// func graphqlHandler() gin.HandlerFunc {
-// 	// NewExecutableSchema and Config are in the generated.go file
-// 	// Resolver is in the resolver.go file
-// 	h := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+func graphqlHandler() gin.HandlerFunc {
+	// NewExecutableSchema and Config are in the generated.go file
+	// Resolver is in the resolver.go file
+	h := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
 
-// 	return func(c *gin.Context) {
-// 		h.ServeHTTP(c.Writer, c.Request)
-// 	}
-// }
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
 
 // // Defining the Playground handler
-// func playgroundHandler() gin.HandlerFunc {
-// 	h := playground.Handler("GraphQL", "/query")
+func playgroundHandler() gin.HandlerFunc {
+	h := playground.Handler("GraphQL", "/query")
 
-// 	return func(c *gin.Context) {
-// 		h.ServeHTTP(c.Writer, c.Request)
-// 	}
-// }
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
 
-// func main() {
-// 	// Setting up Gin
-// 	r := gin.Default()
-// 	r.POST("/query", graphqlHandler())
-// 	r.GET("/", playgroundHandler())
-// 	r.Run()
-// }
+func GinContextToContextMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx := context.WithValue(c.Request.Context(), "GinContextKey", c)
+		c.Request = c.Request.WithContext(ctx)
+		c.Next()
+	}
+}
 
-// func GinContextToContextMiddleware() gin.HandlerFunc {
-// 	return func(c *gin.Context) {
-// 		ctx := context.WithValue(c.Request.Context(), "GinContextKey", c)
-// 		c.Request = c.Request.WithContext(ctx)
-// 		c.Next()
-// 	}
-// }
-
-// func GinContextFromContext(ctx context.Context) (*gin.Context, error) {
-// 	ginContext := ctx.Value("GinContextKey")
-// 	if ginContext == nil {
-// 		err := fmt.Errorf("could not retrieve gin.Context")
-// 		return nil, err
-// 	}
-
-// 	gc, ok := ginContext.(*gin.Context)
-// 	if !ok {
-// 		err := fmt.Errorf("gin.Context has wrong type")
-// 		return nil, err
-// 	}
-// 	return gc, nil
-// }
+func GinContextFromContext(ctx context.Context) (*gin.Context, error) {
+	ginContext := ctx.Value("GinContextKey")
+	if ginContext == nil {
+		err := fmt.Errorf("could not retrieve gin.Context")
+		return nil, err
+	}
+	gc, ok := ginContext.(*gin.Context)
+	if !ok {
+		err := fmt.Errorf("gin.Context has wrong type")
+		return nil, err
+	}
+	return gc, nil
+}
---
+++ backend/postgres/models.go
@@ -0,0 +1,59 @@
+package postgres
+
+import "time"
+
+type Comment struct {
+	ID        int       `gorm:"primaryKey;type:autoIncrement"`
+	PostID    int       `gorm:"not null;default:0"`
+	UID       int       `gorm:"not null;default:0"`
+	Body      string    `gorm:"not null;default:''"`
+	CreatedAt time.Time `gorm:"autoCreateTime"`
+	UpdatedAt time.Time `gorm:"autoUpdateTime"`
+}
+
+type Marker struct {
+	ID        int       `gorm:"primaryKey;type:autoIncrement"`
+	PostID    int       `gorm:"not null;default:0"`
+	Title     string    `gorm:"not null;default:''"`
+	Lat       string    `gorm:"not null;default:''"`
+	Lng       string    `gorm:"not null;default:''"`
+	CreatedAt time.Time `gorm:"autoCreateTime"`
+	UpdatedAt time.Time `gorm:"autoUpdateTime"`
+}
+
+type Post struct {
+	ID        int       `gorm:"primaryKey;type:autoIncrement"`
+	UID       int       `gorm:"not null;default:0"`
+	Title     string    `gorm:"not null;default:''"`
+	Body      string    `gorm:"not null;default:''"`
+	Img       string    `gorm:"not null;default:''"`
+	Liked     []int     `gorm:"not null;default:[]"`
+	CreatedAt time.Time `gorm:"autoCreateTime"`
+	UpdatedAt time.Time `gorm:"autoUpdateTime"`
+}
+
+type Request struct {
+	ID           int       `gorm:"primaryKey;type:autoIncrement"`
+	RequestUID   int       `gorm:"not null;default:0"`
+	RequestedUID int       `gorm:"not null;default:0"`
+	Status       string    `gorm:"not null;default:''"`
+	CreatedAt    time.Time `gorm:"autoCreateTime"`
+	UpdatedAt    time.Time `gorm:"autoUpdateTime"`
+}
+
+type User struct {
+	ID           int       `gorm:"primaryKey;type:autoIncrement"`
+	Email        string    `gorm:"unique;"`
+	Password     string    `gorm:"not null;default:''"`
+	Type         string    `gorm:"not null;default:''"`
+	Session      string    `gorm:"not null;default:''"`
+	Name         string    `gorm:"not null;default:''"`
+	Age          int       `gorm:"not null;default:0"`
+	Gender       string    `gorm:"not null;default:''"`
+	Avatar       string    `gorm:"not null;default:''"`
+	Introduction string    `gorm:"not null;default:''"`
+	Friends      []int     `gorm:"not null;default:[]"`
+	Mute         []int     `gorm:"not null;default:[]"`
+	CreatedAt    time.Time `gorm:"autoCreateTime"`
+	UpdatedAt    time.Time `gorm:"autoUpdateTime"`
+}
