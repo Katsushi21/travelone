89da15e0511b82af284abfb1d23989d51e2f0403
Katsushi21 <sheoaspendie@gmail.com>
2022-04-03 15:21:54 +0900

    update: ログ機能改良 update: DB接続処理分離

---
+++ server/database/connect.go
@@ -0,0 +1,36 @@
+package database
+
+import (
+	"fmt"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func Connect() *gorm.DB {
+	user := os.Getenv("POSTGRES_USER")
+	password := os.Getenv("POSTGRES_PASSWORD")
+	dbname := os.Getenv("POSTGRES_DBNAME")
+	dsn := fmt.Sprintf(
+		"host=%s port=%d user=%s dbname=%s password=%s sslmode=disable",
+		"travel-db.com", 5432, user, dbname, password,
+	)
+	db, err := gorm.Open(
+		postgres.Open(dsn),
+		&gorm.Config{},
+	)
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	db.AutoMigrate(
+		Comments{},
+		Markers{},
+		Posts{},
+		Requests{},
+		Users{},
+	)
+
+	return db
+}
