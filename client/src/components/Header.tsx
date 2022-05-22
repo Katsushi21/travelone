150f0a68e32630387188cdda17afb877f200e950
Katsushi21 <sheoaspendie@gmail.com>
2022-05-22 12:12:36 +0900

    update: ロゴ作成

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Logo } from './Logo'
 
-export const Header = () => {
+export const Header: React.FC = () => {
   return (
     <header>
-      <div className='flex  items-center space-x-2 md:space-x-10'>home</div>
+      <div className='flex items-center space-x-2 md:space-x-10'>
+        <Logo />
+      </div>
     </header>
   )
 }
---
+++ client/src/components/Logo.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Image from 'next/image'
+
+export const Logo: React.FC = () => {
+  return (
+    <div>
+      <Image src='/icon.jpg' height={144} width={144} alt='Logo' />
+    </div>
+  )
+}
