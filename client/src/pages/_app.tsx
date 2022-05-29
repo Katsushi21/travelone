5d8e96425949ecda4d66fe3dcbab6bfbd72946d6
Katsushi21 <sheoaspendie@gmail.com>
2022-05-29 16:55:32 +0900

    update: リクエスト先を変更

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,8 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { createClient, Provider } from 'urql'
+
+console.log(process.env.NEXT_PUBLIC_URQL_CLIENT)
+const client = createClient({
+  url: process.env.NEXT_PUBLIC_URQL_CLIENT,
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Provider value={client}>
+      <Component {...pageProps} />
+    </Provider>
+  )
 }
 
 export default MyApp
