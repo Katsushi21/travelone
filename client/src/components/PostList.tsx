a84af80f12c6702462a05fb4969800d121ceeedc
Katsushi21 <sheoaspendie@gmail.com>
2022-06-18 17:15:38 +0900

    update: daisyUII導入

--- client/src/components/PostList.tsx
+++ client/src/components/PostList.tsx
@@ -4,6 +4,5 @@ import { useGetAllPostsQuery } from 'src/generated/graphql';
 
 export const PostList: React.FC = () => {
   const [result] = useGetAllPostsQuery();
-  console.log(result);
   return <div>PostList</div>;
 };
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -15,22 +15,16 @@ module.exports = {
       ja: [...defaultTheme.fontFamily.sans],
       en: [...defaultTheme.fontFamily.sans],
     },
-    extend: {
-      colors: {
-        theme: {
-          light: '#ffffff',
-          medium: '#cccccc',
-          DEFAULT: '#242424',
-          dark: '#111111',
-        },
-        primary: {
-          // light: '',
-          // medium: '',
-          DEFAULT: '#242424',
-          // dark: '',
-        },
-      },
-    },
   },
-  plugins: [],
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: '',
+    darkTheme: 'dark',
+  },
+  plugins: [require('daisyui')],
 };
