e163d80846716db8bdfebf6861b7c120404bb970
Katsushi21 <sheoaspendie@gmail.com>
2022-06-29 23:38:08 +0900

    update: postlist更新

--- client/src/components/PostList.tsx
+++ client/src/components/PostList.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
 import { useGetAllPostsQuery } from 'src/generated/graphql';
+import { Post } from './Post';
 
 export const PostList: React.FC = () => {
   const [result] = useGetAllPostsQuery();
-  return <div>PostList</div>;
+  return (
+    <div>
+      <Post />
+    </div>
+  );
 };
