3c07117fdf2d3a925c7d4f861110f271433a09ea
Katsushi21 <sheoaspendie@gmail.com>
2022-09-03 15:18:20 +0900

    update: 自動生成前

--- app/ent/entc.go
+++ app/ent/entc.go
@@ -20,19 +20,3 @@ func main() {
 		log.Fatalf("running ent codegen: %v", err)
 	}
 }
-
-func EnsureStructTag(name string) gen.Hook {
-	return func(next gen.Generator) gen.Generator {
-		return gen.GenerateFunc(func(g *gen.Graph) error {
-			for _, node := range g.Nodes {
-				for _, field := range node.Fields {
-					tag := reflect.StructTag(field.StructTag)
-					if _, ok := tag.Lookup(name); !ok {
-						return fmt.Errorf("struct tag %q is missing for field %s.%s", name, node.Name, field.Name)
-					}
-				}
-			}
-			return next.Generate(g)
-		})
-	}
-}
